b725b02727df6279058976d52100118542fc211c
HemmyEvo <141856365+HemmyEvo@users.noreply.github.com>
2025-05-23 13:21:26 +0100

    Update page.tsx

--- app/(root)/classmate/page.tsx
+++ app/(root)/classmate/page.tsx
@@ -78,7 +78,7 @@ const Users = () => {
             {/* User cards with holographic effect */}
             <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-5">
                 {paginatedUsers.map((user) => {
-                    const chat = identity.find(chat => 
+                    const chat = me?._id && identity.find(chat => 
                         chat.participant.includes(me._id) && 
                         chat.participant.includes(user._id)
                     );
